4045601ea59719ff9da20ab6ec99d80905df50d4
Austin <>
2018-05-01 17:39:49 -0600

    Created seed file with default post content

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+100.times do |post|
+	Post.create!(date: Date.today, rationale: "#{post} raitionale content")
+end
+
+puts "100 posts have been created"
